1087c0f1125d9b02e3e799d62f2fe414940d8877
nuelladev <emmanuellaokafor42@yahoo.com>
2024-01-02 05:07:53 +0100

    feature: added authority and post models

--- src/main/java/com/example/MyBlog/models/Post.java
+++ src/main/java/com/example/MyBlog/models/Post.java
@@ -1,4 +1,41 @@
 package com.example.MyBlog.models;
 
+
+
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Builder
+@AllArgsConstructor
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    Long id;
+
+    String title;
+
+    @Column(columnDefinition = "TEXT")
+    String body;
+
+    LocalDateTime createdAt;
+
+    LocalDateTime updatedAt;
+
+    String imageFilePath;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    User user;
+
 }
