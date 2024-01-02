b28b3cfd067fae4bf81a18def72f1b5e7bbf23f0
nuelladev <emmanuellaokafor42@yahoo.com>
2024-01-02 05:55:27 +0100

    Feature: added entities for Reply and like

--- src/main/java/com/example/MyBlog/models/Like.java
+++ src/main/java/com/example/MyBlog/models/Like.java
@@ -1,4 +1,26 @@
 package com.example.MyBlog.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "comment_id", referencedColumnName = "id", nullable = false)
+    private Comment comment;
+
+
 }
+
--- src/main/java/com/example/MyBlog/models/Reply.java
+++ src/main/java/com/example/MyBlog/models/Reply.java
@@ -1,4 +1,28 @@
 package com.example.MyBlog.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Reply {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String replyText;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "comment_id", referencedColumnName = "id", nullable = false)
+    private Comment comment;
+
+
 }
+
