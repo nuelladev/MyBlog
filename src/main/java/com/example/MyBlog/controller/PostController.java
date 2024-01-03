17e3bcfe31a8a2546a1a3783e863eea483fdffa1
nuelladev <emmanuellaokafor42@yahoo.com>
2024-01-03 15:54:35 +0100

    added implementations for PostController

--- src/main/java/com/example/MyBlog/controller/PostController.java
+++ src/main/java/com/example/MyBlog/controller/PostController.java
@@ -1,4 +1,132 @@
 package com.example.MyBlog.controller;
 
+import com.example.MyBlog.models.Post;
+import com.example.MyBlog.models.User;
+import com.example.MyBlog.services.FileService;
+import com.example.MyBlog.services.PostService;
+import com.example.MyBlog.services.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.security.Principal;
+import java.util.Optional;
+
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
 public class PostController {
+
+    private final PostService postService;
+    private final UserService userService;
+    private final FileService fileService;
+
+    @GetMapping("/posts/{id}")
+    public String getPost(@PathVariable Long id, Model model) {
+
+
+        Optional<Post> optionalPost = this.postService.getById(id);
+
+
+        if (optionalPost.isPresent()) {
+            Post post = optionalPost.get();
+            model.addAttribute("post", post);
+            return "post";
+        } else {
+            return "404";
+        }
+    }
+
+    @PostMapping("/posts/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public String updatePost(@PathVariable Long id, Post post, @RequestParam("file") MultipartFile file) {
+
+        Optional<Post> optionalPost = postService.getById(id);
+        if (optionalPost.isPresent()) {
+            Post existingPost = optionalPost.get();
+
+            existingPost.setTitle(post.getTitle());
+            existingPost.setBody(post.getBody());
+
+            try {
+                fileService.saveFile(file);
+                existingPost.setImageFilePath(file.getOriginalFilename());
+            } catch (Exception e) {
+                log.error("Error processing file: {}");
+            }
+
+            postService.savePost(existingPost);
+        }
+
+        return "redirect:/posts/" + post.getId();
+    }
+
+    @GetMapping("/posts/new")
+    @PreAuthorize("isAuthenticated()")
+    public String createNewPost(Model model) {
+
+        Post post = new Post();
+        model.addAttribute("post", post);
+        return "post_new";
+    }
+
+    @PostMapping("/posts/new")
+    @PreAuthorize("isAuthenticated()")
+    public String createNewPost(@ModelAttribute Post post, @RequestParam("file") MultipartFile file, Principal principal) {
+        String authUsername = "anonymousUser";
+        if (principal != null) {
+            authUsername = principal.getName();
+        }
+
+        User user = userService.findOneByEmail(authUsername).orElseThrow(() -> new IllegalArgumentException("Account not found"));
+
+        try {
+            fileService.saveFile(file);
+            post.setImageFilePath(file.getOriginalFilename());
+        } catch (Exception e) {
+            log.error("Error processing file: {}");
+        }
+
+        post.setUser(user);
+        postService.savePost(post);
+        return "redirect:/";
+    }
+
+    @GetMapping("/posts/{id}/edit")
+    @PreAuthorize("isAuthenticated()")
+    public String getPostForEdit(@PathVariable Long id, Model model) {
+
+        // find post by id
+        Optional<Post> optionalPost = postService.getById(id);
+        // if post exist put it in model
+        if (optionalPost.isPresent()) {
+            Post post = optionalPost.get();
+            model.addAttribute("post", post);
+            return "post_edit";
+        } else {
+            return "404";
+        }
+    }
+
+    @GetMapping("/posts/{id}/delete")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public String deletePost(@PathVariable Long id) {
+
+
+        Optional<Post> optionalPost = postService.getById(id);
+        if (optionalPost.isPresent()) {
+            Post post = optionalPost.get();
+
+            postService.deletePost(post);
+            return "redirect:/";
+        } else {
+            return "404";
+        }
+    }
+
 }
