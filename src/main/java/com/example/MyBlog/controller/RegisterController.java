772ef055612668ea687f2d4e124d073e18e0da7a
nuelladev <emmanuellaokafor42@yahoo.com>
2024-01-03 15:57:04 +0100

    completed the user implementatio for Registeration Controller

--- src/main/java/com/example/MyBlog/controller/RegisterController.java
+++ src/main/java/com/example/MyBlog/controller/RegisterController.java
@@ -1,4 +1,33 @@
 package com.example.MyBlog.controller;
 
+
+import com.example.MyBlog.models.User;
+import com.example.MyBlog.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class RegisterController {
+
+    private final UserService userService;
+
+    @GetMapping("/register")
+    public String getRegisterForm(Model model) {
+
+        User user = new User();
+        model.addAttribute("user", user);
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerNewUser(@ModelAttribute User user) {
+        userService.saveUser(user);
+        return "redirect:/";
+    }
+
 }
