13ebb2cb0a8efec3c7ed97351f6c4a96a3abb666
nuelladev <emmanuellaokafor42@yahoo.com>
2024-01-03 15:42:13 +0100

    added a few controller - home, image, and login

--- src/main/java/com/example/MyBlog/config/WebSecurityConfig.java
+++ src/main/java/com/example/MyBlog/config/WebSecurityConfig.java
@@ -1,4 +1,38 @@
 package com.example.MyBlog.config;
 
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeRequests(auth -> auth
+                        .requestMatchers("/css/**", "/js/**", "/images/**", "/fonts/**", "/webjars/**", "/", "/rss/**", "/register/**", "/posts/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .usernameParameter("email")
+                        .passwordParameter("password")
+                        //.defaultSuccessURL("/")
+                        //.failureURL("/login?error")
+                        .permitAll()
+                );
+
+        return http.build();
+    }
 }
+
---
+++ src/main/java/com/example/MyBlog/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.example.MyBlog.controller;
+
+import com.example.MyBlog.models.Post;
+import com.example.MyBlog.services.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class HomeController {
+
+    private final PostService postService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<Post> posts = postService.getAll();
+        model.addAttribute("posts", posts);
+        return "home";
+    }
+
+}
