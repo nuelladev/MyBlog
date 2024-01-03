a0592e111cb6817900eceafc3571c942dc76da60
nuelladev <emmanuellaokafor42@yahoo.com>
2024-01-03 15:44:00 +0100

    added layers for login

--- src/main/java/com/example/MyBlog/controller/Login.java
+++ src/main/java/com/example/MyBlog/controller/Login.java
@@ -1,4 +1,14 @@
 package com.example.MyBlog.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class Login {
+
+    @GetMapping("/login")
+    public String getLogin() {
+
+        return "login";
+    }
 }
